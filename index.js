2fc342b052527de7112fd3e8db68bb00854fcd1e
kiwi <kiwi@Aman.local>
2017-05-10 22:40:10 +0530

    Add commander to provide option

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ var clear = require('clear');
 var files = require('./lib/files');
 var chalk = require('chalk');
 var figlet = require('figlet');
+var vtool = require('svn-spawn');
+var program = require('commander');
+
+var repoClient;
 
 clear();
-console.log(
-    chalk.yellow(
-        figlet.textSync('GenKit', { horizontalLayout: 'full' })
-    )
+logger(
+    {
+        color: 'yellow',
+        message: figlet.textSync('GenKit', { horizontalLayout: 'full' })
+    }
 );
 
 var repoQuestion = [
@@ -41,17 +46,78 @@ var repoQuestion = [
 ];
 
 function notEmpty( val ) {
-    return !val ? false : true;
+    if ( !val ) {
+        logger( { color: 'red', message: 'Required field!!' } );
+        return false;
+    }
+    return true;
 }
 
-inquirer.prompt( repoQuestion ).then(
-    function ( inputs ) {
-        console.log(
-            chalk.green('successfully recieved all values')
-        );
+function setupSVN ( config ) {
+    repoClient = new vtool(
+        {
+            cwd: config.repo,
+            username: config.username,
+            password: config.password,
+            noAuthCache: true
+        }
+    );
+logger(repoClient);
+    repoClient.getInfo(function(e,d){console.log(e,d)});
+}
 
-        console.log(inputs);
-
+function logger( options ) {
+    var setLogColor;
+    if ( options.hasOwnProperty('color') && options.color ) {
+        setLogColor = chalk[ options.color ];
     }
-);
+
+    var message;
+    if ( typeof setLogColor == 'function' ) {
+        message = setLogColor( options.message );
+    } else {
+        message = options;
+    }
+    
+    if ( message ) {
+        console.log( message );
+    }
+}
+
+program
+    .version('0.0.1')
+    .command('run')
+    .action(function(){
+
+        inquirer.prompt( repoQuestion ).then(
+            function ( inputs ) {
+                logger( { color: 'green', message:'successfully recieved all values' } );
+                logger( inputs );
+                logger(inputs.versioningtool.toLowerCase() == 'svn');
+                if ( inputs.repo.toLowerCase() == 'svn' ) {
+                    setupSVN( inputs );
+                    getSVNInfo();
+                }
+            }
+        );
+    });
+
+function getSVNInfo() {
+    repoClient.getInfo(
+        function(err, data) {
+                                console.log('subcommand done', err, data);
+            try{
+            if ( err ) {
+                repoClient.cmd(['co', '--option1=xx', '--option2=xx', 'arg1', 'arg2'], function(err, data) {
+                    console.log('subcommand done', err, data);
+                });
+            }}catch(e) {
+                logger(e);
+            }
+            console.log('Repository url is %s', data.url);
+        }
+    );
+}
+
+program.parse(process.argv);
 
