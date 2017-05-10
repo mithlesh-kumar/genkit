8ca2ed22e6b376fe528d1967ff7abf0751c365d0
Mithlesh Kumar <mithlesh.kumar@kiwitech.com>
2017-05-10 17:55:40 +0530

    Add validation check

--- index.js
+++ index.js
@@ -13,28 +13,45 @@ console.log(
     )
 );
 
-var authQuestion = [
+var repoQuestion = [
+    {
+        name: 'versioningtool',
+        type: 'input',
+        message: 'Enter your versioning tool:',
+        validate: function( value ) { return notEmpty( value ); }
+    },
     {
         name: 'username',
         type: 'input',
         message: 'Enter your username:',
-        validate: function( value ) { return true; }
+        validate: function( value ) { return notEmpty( value ); }
     },
     {
         name: 'password',
         type: 'password',
         message: 'Enter your password:',
-        validate: function( value ) { return true; }
+        validate: function( value ) { return notEmpty( value ); }
+    },
+    {
+        name: 'repo',
+        type: 'input',
+        message: 'Enter project repo url:',
+        validate: function( value ) { return notEmpty( value ); }
     }
 ];
 
-inquirer.prompt(authQuestion).then(
-    function () {
+function notEmpty( val ) {
+    return !val ? false : true;
+}
+
+inquirer.prompt( repoQuestion ).then(
+    function ( inputs ) {
         console.log(
             chalk.green('successfully recieved all values')
         );
 
-        
+        console.log(inputs);
+
     }
 );
 
